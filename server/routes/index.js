13ef86fb0e29a0ec3efe15a8e176a87e6210e83a
SorinGFS <sorin.gfs@protonmail.com>
2022-10-16 02:03:12 +0300

    moving utility functions to own repository zerodep

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 'use strict';
 // https://expressjs.com/en/4x/api.html#express.router
 const router = require('express').Router();
-const fs = require('webaccess-base/fs');
+const fs = require('zerodep/node/fs');
 
 fs.dirs(__dirname).forEach((route) => router.use(`/${route}/`, require(`./${route}`)));
 
